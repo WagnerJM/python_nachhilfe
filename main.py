1dabdb3dbdf05157042f655ff5883d519fa1000c
WagnerJM <45420760+WagnerJM@users.noreply.github.com>
2021-05-11 14:21:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ csv_data = csv.reader(file, delimiter=";")
 csv_data = csv.reader(open('data/data.csv', 'r'), delimiter=';')
 
 for data in csv_data:
-    print(f'Zeit: {data[5]} Temp: {data[6]}')
-
-
+    print(f'Zeit: {data[5]} Temp: {data[6]}'
+#veränderung
 
